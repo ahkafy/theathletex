552aed27630643e83073d091342fc947ca696226
abdullah hel kafy <ahkafy@gmail.com>
2025-12-02 13:21:41 +0600

    Fix transaction search in main query

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -193,7 +193,6 @@ class DashboardController extends Controller
         if ($search) {
             $transactionsQuery->where(function($query) use ($search) {
                 $query->where('transaction_id', 'like', '%' . $search . '%')
-                      ->orWhere('gateway_transaction_id', 'like', '%' . $search . '%')
                       ->orWhereHas('participant', function($q) use ($search) {
                           $q->where('name', 'like', '%' . $search . '%')
                             ->orWhere('email', 'like', '%' . $search . '%')
